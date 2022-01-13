90b717f92f5742c048951bb209e26e78d1cf5e05
MaxSMokeSkaarj <maxsmokeskaaj@gmail.com>
2022-01-13 18:31:23 +1000

    Промежуточный коммит

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const { Client, Intents } = require('discord.js');
 const robot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILD_PRESENCES] });
-const {sender} = require('./lib/utils.js');
-const {chats} = require('./json/config.json')
+const {sender,editor} = require('./lib/utils.js');
+const {chats} = require('./json/config.json');
 
 robot.on("ready", function() {
 	console.log(robot.user.username + " is running");
@@ -14,8 +14,14 @@ robot.on('messageCreate', (msg) => {
 	if (msg.author.username != robot.user.username && msg.author.discriminator != robot.user.discriminator) {
 		if (chats.includes(msg.channelId)) {
 			sender(msg);
-		}
+		};
 	};
 });
+
+/*robot.on("messageUpdate", (msg) => {
+	if (msg.author.username != robot.user.username && msg.author.discriminator != robot.user.discriminator) {
+		editor(msg);
+	}
+});*/
 
 robot.login(process.env.DISCORD_TOKEN);
